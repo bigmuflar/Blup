4cd1ad2069fd4075aee3ce2854528090506ace5f
bigmuflar <jmdegonda@gmail.com>
2016-11-17 16:12:01 -0700

    major updates

--- public/js/controller.dashboard.js
+++ public/js/controller.dashboard.js
@@ -3,32 +3,38 @@ angular.module('module.dashboard', [])
 
 dashboard.$inject = ['clientFactory','$http'];
 
-function dashboard(clientFactory, $http){
-    var dashboard = this;
+function getExhibit(clientFactory, $http){
+  var getExhibit = this;
 
-    console.info('Dashboard initialized');
+  console.info('Dashboard initialized');
 
-    clientFactory.getExhibit().then(function(success){
-      getExhibit.data = success.data.data;
-      // console.log("dashboard data", getExhibit.data);
-      // console.log('API is working', success);
-    },  function(error){
-      console.log('error on get API', error);
-    });
+  clientFactory.getExhibit().then(function(success){
+    getExhibit.data = success.data.data;
+    // console.log("dashboard data", getExhibit.data);
+    // console.log('API is working', success);
+  },  function(error){
+    console.log('error on get API', error);
+  });
+};
 
-    clientFactory.getObject().then(function(success){
-      getObject.data = success.data.data;
-      console.log("Object Data", getObject.data);
-    }, function(error){
-      console.log('error on get Object API', error);
-    });
+function getObject(clientFactory, $http){
+  var getObject = this;
 
-    clientFactory.getUser().then(function(success){
-      getUser.data = success.data.data;
-      console.log("getUser Data", getUser.data);
-    }, function(error){
-      console.log('error on API', error);
-    });
+  clientFactory.getObject().then(function(success){
+    getObject.data = success.data.data;
+    console.log("Object Data", getObject.data);
+  }, function(error){
+    console.log('error on get Object API', error);
+  });
+};
 
+function getUser(clientFactory, $http){
+  var getUser = this;
 
-}
+  clientFactory.getUser().then(function(success){
+    getUser.data = success.data.data;
+    console.log("getUser Data", getUser.data);
+  }, function(error){
+    console.log('error on API', error);
+  });
+};
