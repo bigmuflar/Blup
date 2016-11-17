e7a82918a17df5c916dd21a2c0d5442095c9b921
bigmuflar <jmdegonda@gmail.com>
2016-11-17 12:08:14 -0700

    update

--- public/js/module.dashboard.js
+++ public/js/module.dashboard.js
@@ -1,11 +1,12 @@
 angular.module('module.dashboard', [])
     .controller('DashboardController', dashboard);
 
-dashboard.$inject = ['$http','clientFactory'];
+dashboard.$inject = ['clientFactory','$http'];
 
-function dashboard($http, clientFactory){
+function dashboard(clientFactory, $http){
     console.info('Dashboard initialized');
     clientFactory.getAPIData().then(function(success){
+      dashboard.api = success;
       console.log('API is working', success);
     },  function(error){
       console.log('error on API', error);
