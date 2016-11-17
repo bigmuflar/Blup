00e6379535c58118a0ce8f5732391b9fed9e9edb
bigmuflar <jmdegonda@gmail.com>
2016-11-17 16:17:10 -0700

    major updates

--- public/js/controller.dashboard.js
+++ public/js/controller.dashboard.js
@@ -3,38 +3,40 @@ angular.module('module.dashboard', [])
 
 dashboard.$inject = ['clientFactory','$http'];
 
-function getExhibit(clientFactory, $http){
-  var getExhibit = this;
+function dashboard(clientFactory, $http){
+    function getExhibit(clientFactory, $http){
+      var getExhibit = this;
 
-  console.info('Dashboard initialized');
+      console.info('Dashboard initialized');
 
-  clientFactory.getExhibit().then(function(success){
-    getExhibit.data = success.data.data;
-    // console.log("dashboard data", getExhibit.data);
-    // console.log('API is working', success);
-  },  function(error){
-    console.log('error on get API', error);
-  });
-};
+      clientFactory.getExhibit().then(function(success){
+        getExhibit.data = success.data.data;
+        // console.log("dashboard data", getExhibit.data);
+        // console.log('API is working', success);
+      },  function(error){
+        console.log('error on get API', error);
+      });
+    };
 
-function getObject(clientFactory, $http){
-  var getObject = this;
+    function getObject(clientFactory, $http){
+      var getObject = this;
 
-  clientFactory.getObject().then(function(success){
-    getObject.data = success.data.data;
-    console.log("Object Data", getObject.data);
-  }, function(error){
-    console.log('error on get Object API', error);
-  });
-};
+      clientFactory.getObject().then(function(success){
+        getObject.data = success.data.data;
+        console.log("Object Data", getObject.data);
+      }, function(error){
+        console.log('error on get Object API', error);
+      });
+    };
 
-function getUser(clientFactory, $http){
-  var getUser = this;
+    function getUser(clientFactory, $http){
+      var getUser = this;
 
-  clientFactory.getUser().then(function(success){
-    getUser.data = success.data.data;
-    console.log("getUser Data", getUser.data);
-  }, function(error){
-    console.log('error on API', error);
-  });
+      clientFactory.getUser().then(function(success){
+        getUser.data = success.data.data;
+        console.log("getUser Data", getUser.data);
+      }, function(error){
+        console.log('error on API', error);
+      });
+    };
 };
