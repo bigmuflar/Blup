4b9b8b29e02bafe872aafb336f9f3b1a3a6ff742
bigmuflar <jmdegonda@gmail.com>
2016-11-17 17:15:47 -0700

    major updates

--- public/js/controller.dashboard.js
+++ public/js/controller.dashboard.js
@@ -7,37 +7,24 @@ function dashboard(clientFactory, $http){
     var dashboard = this;
     console.log('hitting dashboard');
 
-    this.exhibit = function(clientFactory, $http){
-      var exhibit = this;
-      console.info('exhibit initialized');
-
       clientFactory.getExhibit().then(function(success){
-        exhibit.data = success.data.data;
+        dashboard.exhibits = success.data.data;
         // console.log("dashboard data", getExhibit.data);
         // console.log('API is working', success);
       },  function(error){
         console.log('error on get API', error);
       });
-    };
-
-    this.artifact = function(clientFactory, $http){
-      var artifact = this;
-      console.info('artifact initialized');
 
       clientFactory.getObject().then(function(success){
-        artifact.data = success.data.data;
+        dashboard.artifacts = success.data.data;
         console.log("Object Data", artifact.data);
       }, function(error){
         console.log('error on get Object API', error);
       });
     };
 
-    this.user = function(clientFactory, $http){
-      var user = this;
-      console.info('getUser initialized');
-
       clientFactory.getUser().then(function(success){
-        user.data = success.data.data;
+        dashboard.users = success.data.data;
         console.log("getUser Data", user.data);
       }, function(error){
         console.log('error on API', error);
