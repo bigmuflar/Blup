8c748e5b3c86b2c56dcbca85f095b56db510aaf5
bigmuflar <jmdegonda@gmail.com>
2016-11-18 15:50:14 -0700

    major updates

--- public/js/controller.dashboard.js
+++ public/js/controller.dashboard.js
@@ -1,5 +1,5 @@
 angular.module('module.dashboard', [])
-    .controller('floorCtrl', fCtrl),
+    .controller('floorCtrl', fCtrl)
     .controller('DashboardController', dashboard);
 
 dashboard.$inject = ['clientFactory','$http'];
@@ -32,5 +32,5 @@ function dashboard(clientFactory, $http){
 
 function fCtrl() {
   var fCtrl = this;
-  this.floors = [1, 2, 3, 4, 5];
+  this.floors = ['1', '2', '3', '4', '5'];
 };
