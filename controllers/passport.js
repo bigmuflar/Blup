decdbe1948ba623523ede78fe132d97a025ed41b
bigmuflar <jmdegonda@gmail.com>
2016-11-19 12:55:38 -0700

    update

--- controllers/passport.js
+++ controllers/passport.js
@@ -1,15 +1,15 @@
-var createTemplate = require("passbook"),
-    template = createTemplate("coupon", {
-      passTypeIdentifier: "pass.com.example.passbook",
-      teamIdentifier:     "MXL",
-      backgroundColor:   "rgb(255,255,255)"
-    });
-
-    template.keys("/etc/passbook/keys", "secret");
-    template.loadImagesFrom("images");
-
-    template.fields.passTypeIdentifier = "pass.com.beacons.blup";
-
-    console.log(template.passTypeIdentifier());
-
-    template.teamIdentifier("MXL").passTypeIdentifier("pass.com.beacons.blup")
+// var createTemplate = require("passbook"),
+//     template = createTemplate("coupon", {
+//       passTypeIdentifier: "pass.com.example.passbook",
+//       teamIdentifier:     "MXL",
+//       backgroundColor:   "rgb(255,255,255)"
+//     });
+//
+//     template.fields.passTypeIdentifier = "pass.com.beacons.blup";
+//
+//     console.log(template.passTypeIdentifier());
+//
+//     template.teamIdentifier("MXL").passTypeIdentifier("pass.com.beacons.blup");
+//
+//     template.keys("/etc/passbook/keys", "secret");
+//     template.loadImagesFrom("images");
