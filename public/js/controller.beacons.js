d057e55e7a245fcf43afe6fbe856f879a8ce9f4e
bigmuflar <jmdegonda@gmail.com>
2016-11-23 13:14:36 -0700

    update

--- public/js/controller.beacons.js
+++ public/js/controller.beacons.js
@@ -1,5 +1,16 @@
 angular.module('module.dashboard', [])
-    .controller('BeaconController', beaconCtrl);
+    .controller('BeaconController', beaconCtrl)
+    .directive('errSrc', function() {
+      return {
+        link: function(scope, element, attrs) {
+          element.bind('error', function() {
+            if (attrs.src != attrs.errSrc) {
+              attrs.$set('src', attrs.errSrc);
+            }
+          });
+        }
+      }
+    });
 
 beaconCtrl.$inject = ['clientFactory','$http'];
 
