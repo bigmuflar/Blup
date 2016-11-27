e3b0564a34a109aca0738880eec0072c5ce0d3ce
bigmuflar <jmdegonda@gmail.com>
2016-11-27 16:31:13 -0700

    update

--- routes.js
+++ routes.js
@@ -13,9 +13,9 @@ module.exports = function(app) {
         res.render('home');
     });
     app.get('/login', Auth.render); // route for the login page
-    app.get('/logout', Auth.logout); // route for logging out
+    app.get('/', Auth.logout); // route for logging out
 
-    app.post('/login', Auth.login); // form request emdpoint for loggin in
+    app.post('/login', Auth.login); // form request endpoint for loggin in
     app.post('/register', Auth.register); // form request endpoint for user registration
 
     // DAHSBOARD
