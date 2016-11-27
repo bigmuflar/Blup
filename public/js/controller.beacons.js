59dfb688a8dd4e55453703cb159cd64d37b247da
bigmuflar <jmdegonda@gmail.com>
2016-11-27 09:18:23 -0700

    Update

--- public/js/controller.beacons.js
+++ public/js/controller.beacons.js
@@ -45,7 +45,7 @@ function speakit(beaconCtrl, $http){
       audio = document.getElementById('audio'),
       wavsource = document.getElementById('wavsource'),
       // translate = beaconCtrl.artifacts.description;
-      translate = document.getElementById('translate-text');
+      translate = document.getElementById('translate-text').innerHTML;
 
       wavsource.src = '/api/speak?text='+translate;
 
