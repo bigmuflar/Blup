73f0342350c97ef0c57dab02a3024dc6454b7d3e
bigmuflar <jmdegonda@gmail.com>
2016-11-27 14:29:05 -0700

    Update

--- public/js/controller.dashboard.js
+++ public/js/controller.dashboard.js
@@ -42,13 +42,14 @@ function dashboard(clientFactory, $http){
 function fCtrl() {
   var fCtrl = this;
   this.data = {
-    model: {id: '1'},
+    model: {id: 1},
     availableOptions: [
-      {id: '1', name: '1st Floor'},
-      {id: '2', name: '2nd Floor'},
-      {id: '3', name: '3rd Floor'},
-      {id: '4', name: '4th Floor'},
-      {id: '5', name: '5th Floor'}
+      {id: 1, name: '1st Floor'},
+      {id: 2, name: '2nd Floor'},
+      {id: 3, name: '3rd Floor'},
+      {id: 4, name: '4th Floor'},
+      {id: 5, name: '5th Floor'},
+      {id: 6, name: '6th Floor'},      
     ]
   };
     console.log('hitting floors');
